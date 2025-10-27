7c589fe7be45086b232f63c7529ca6a0078b3a73
ShunTr <shuntr.dev@gmail.com>
2025-10-27 17:36:08 +0100

    feat: Update environment configuration and enhance SEO metadata in layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://pablomartinez.shuntr.dev/', // Actualiza con tu dominio
     vite: {
         plugins: [tailwindcss()],
     },
